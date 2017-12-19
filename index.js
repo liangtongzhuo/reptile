85e8ca45bcf96948b398d510231f0cf12fe5fc0b
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-19 08:50:13 +0800

    更新

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
+'use strict'
 const puppeteer = require('puppeteer');
 
+(async () => {
+  const browser = await puppeteer.launch({headless:false})
+  const page = await browser.newPage()
+  await page.goto('https://baidu.com')
+  await page.type('[name="wd"]','puppeteer',{delay:100})
+  await page.click('#su')
+  await page.waitFor(2000)
+  await browser.close()
+})();
+
 // (async () => {
-//   const browser = await puppeteer.launch({headless:false})
+//   const browser = await puppeteer.launch({ headless: false })
 //   const page = await browser.newPage()
-//   await page.goto('https://baidu.com')
-//   await page.type('[name="wd"]','puppeteer',{delay:100})
-//   await page.click('#su')
-//   await page.waitFor(2000)
-//   await browser.close()
+//   await page.goto('https://search.bilibili.com/all?keyword=%E8%85%BF&from_source=banner_search')
+//   // await page.type('.search-keyword','腿',{delay:100})
+//   await page.waitFor(4000)
+//   await page.click('.nextPage')
+//   await page.waitFor(4000)
+//
+//   await page.waitFor(4000)
+//
+//
+//   const data = await page.evaluate(() => {
+//     const imgs = [...document.querySelectorAll('img')];
+//     return imgs.map((img) => {
+//       return {
+//         src: img.src
+//       }
+//     });
+//   })
+//   console.log('-----',data)
 // })();
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
-  await page.goto('https://search.bilibili.com/all?keyword=%E8%85%BF&from_source=banner_search')
-  // await page.type('.search-keyword','腿',{delay:100})
-  await page.waitFor(4000)
-  await page.click('.nextPage')
-  await page.waitFor(4000)
-
-  await page.waitFor(4000)
-  
-
-  const data = await page.evaluate(() => {
-    const imgs = [...document.querySelectorAll('img')];
-    return imgs.map((img) => {
-      return {
-        src: img.src
-      }
-    });
-  })
-  console.log('-----',data)
-})();
